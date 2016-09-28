4bf73dce56fcd373ebe9a3eab43a1f4e4907618a
DevExchanges <wandered.vip.007@gmail.com>
2016-09-28 14:16:08 +0700

    update code

---
+++ app/src/main/java/info/devexchanges/downloadingfile/MainActivity.java
@@ -0,0 +1,68 @@
+package info.devexchanges.downloadingfile;
+
+import android.app.DownloadManager;
+import android.app.DownloadManager.Query;
+import android.app.DownloadManager.Request;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.webkit.URLUtil;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private long enqueue;
+    private DownloadManager downloadManager;
+    private final static String DOWNLOAD_URL = "http://i.imgur.com/wsibrEw.gif";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                    intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+                    Query query = new Query();
+                    query.setFilterById(enqueue);
+                    Cursor cursor = downloadManager.query(query);
+                    if (cursor.moveToFirst()) {
+                        int columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS);
+                        if (DownloadManager.STATUS_SUCCESSFUL == cursor.getInt(columnIndex)) {
+                            ImageView view = (ImageView) findViewById(R.id.imageView);
+                            String uriString = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                            view.setImageURI(Uri.parse(uriString));
+                            Toast.makeText(MainActivity.this, "Download completed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        };
+
+        registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+    }
+
+    public void onClick(View view) {
+        downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        Request request = new Request(Uri.parse(DOWNLOAD_URL));
+
+        //Get download file name
+        String fileExtenstion = MimeTypeMap.getFileExtensionFromUrl(DOWNLOAD_URL);
+        String name = URLUtil.guessFileName(DOWNLOAD_URL, null, fileExtenstion);
+
+        //Save file to destination folder
+        request.setDestinationInExternalPublicDir("/Downloads", name);
+
+        enqueue = downloadManager.enqueue(request);
+    }
+}
